ad2536d7b962f6305bf00fc62f327dcca71269e1
106523 <102621831+106523@users.noreply.github.com>
2023-01-28 19:41:51 +0000

    Make steptimer

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,13 @@
+
+//Timer init varaibles.
+var StepTime;
+let LastTimeStamp = 0; 
+//Main timer loop.
 setInterval(() => {
     control();
 }, 1);
 function control() {
-    
+    //Step timer.
+    StepTime = Date.now() - LastTimeStamp;
+    LastTimeStamp = Date.now();
 }
