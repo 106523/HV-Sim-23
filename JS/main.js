04f96dd91759c1e2c1914282a5aa33d7a13f047b
106523 <102621831+106523@users.noreply.github.com>
2023-01-28 21:30:44 +0000

    Implement EV mode

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,14 @@
-
+//Powertrain variables.
+let EngineRPM = 0;
+let MG2KW = 0;
+let MG1KW = 0;
+let Speed = 0;
+let TorqueLimit = 0;
+let TorqueDemand = 0;
+let MG1TorqueOutput = 0;
+let EngineTorqueOutput = 0;
 //Timer init varaibles.
-var StepTime;
+let StepTime;
 let LastTimeStamp = 0; 
 //Main timer loop.
 setInterval(() => {
@@ -14,3 +22,18 @@ function control() {
     let AcceleratorInput = document.getElementById("Accelerator");
     //Convert accelerator pedal input to torque.
 }
+//EV drive mode
+function EVMode() {
+    //Pure EV Mode
+    //Dont call the engine ECU to save resources
+    //let everything else know the engine isnt doing shit
+    EngineRPM = 0;
+    MG2KW = 0;
+    EngineTorqueOutput = 0;
+    //why the FUCK did I make MG1Torquelimit an absolute? Did it ever go into the NEGATIVE range for god knows what reason?!
+    if (TorqueDemand >= Math.abs(TorqueLimit)) {
+        MG1TorqueOutput = TorqueLimit;
+      } else {
+        MG1TorqueOutput = TorqueDemand;
+      }
+  }
