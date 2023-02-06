92f9c1e255e8af06b2821873cbe6b469085dc7c2
106523 <102621831+106523@users.noreply.github.com>
2023-02-06 18:13:30 +0000

    comit some more changes from the other project

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,7 @@
 //Initialize the F_CAN bus.
 var F_CAN = {
   "Speed" : 0,
+  "WheelRPM" : 0,
   "MG1RPM" : 0,
   "TorqueDemand" : 0,
   "StepTime" : 0,
@@ -9,8 +10,9 @@ var F_CAN = {
   "MG1TorqueLimit" : 0,
   "MG1Torque" : 0,
   "RegenAvalibleTorque" : 0,
-  "HVSOC" : 0,
+  "HVSOC" : 100,
   "FrictionBrakeDemand" : 0,
+  "BrakeDemand" : 0,
   "HVMode" : 0,
   "LockUpClutch" : 0,
   "EngineGeneration" : 0,
@@ -25,32 +27,21 @@ const MotorShaft = 2.454;
 const OverDrive = 0.806;
 //Timer init varaibles.
 let StepTime;
-let LastTimeStamp = 0; 
+let LastTimeStamp = Date.now(); 
 //Main timer loop.
 setInterval(() => {
     control();
 }, 1);
 function control() {
     //Step timer.
-    StepTime = Date.now() - LastTimeStamp;
+    F_CAN.StepTime = Date.now() - LastTimeStamp;
     LastTimeStamp = Date.now();
     //Speed to MG1RPM.
-    MG1RPM = Speed * 130;
+    F_CAN.MG1RPM = F_CAN.Speed * 130;
     //Input system.
     const AcceleratorInput = document.getElementById("Accelerator");
     //Convert accelerator pedal input to torque.
-    if ((9.5488 * (MG1KW + BatteryMaxPowerDraw)) / F_CAN[9] <= 314) {
-        TorqueLimit = (9.5488 * (MG1KW + BatteryMaxPowerDraw)) / F_CAN[9];
-      } else {
-        TorqueLimit = 314;
-      }
-      if ((9.5488 * 134972) / F_CAN[9] <= 314) {
-        let MG1Torque = (9.5488 * 134972) / F_CAN[9];
-      } else {
-        let MG1Torque = 314;
-      }
-      //Return the Torque Demand
-      TorqueDemand = MG1Torque * (AcceleratorInput / 100);
+    
 }
 //EV drive mode
 function EVMode() {
@@ -60,8 +51,7 @@ function EVMode() {
     EngineRPM = 0;
     MG2KW = 0;
     EngineTorqueOutput = 0;
-    //why the FUCK did I make MG1Torquelimit an absolute? Did it ever go into the NEGATIVE range for god knows what reason?!
-    if (TorqueDemand >= Math.abs(TorqueLimit)) {
+    if (TorqueDemand >= TorqueLimit) {
         MG1TorqueOutput = TorqueLimit;
       } else {
         MG1TorqueOutput = TorqueDemand;
